bd8e0b6df7ec7aeba31360cd9f0951778cc93e00
Evan <evanjo01@gmail.com>
2019-02-27 12:01:17 -0500

    url swapped

--- scripts/solarize-slack.sh
+++ scripts/solarize-slack.sh
@@ -6,7 +6,7 @@
 cat << 'EOF' >> /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
 document.addEventListener('DOMContentLoaded', function() {
  $.ajax({
-   url: 'https://raw.githubusercontent.com/nakedsushi/solarized-slack/master/css/solarized.css',
+   url: 'https://raw.githubusercontent.com/EvanJo/solarized-slack/master/css/solarized.css',
    success: function(css) {
      $("<style></style>").appendTo('head').html(css);
    }
